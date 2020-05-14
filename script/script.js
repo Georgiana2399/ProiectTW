1a5570d75cb452f9cc734753689b2de4c034a54b
Butiuc Georgiana <b.georgiana2399@gmail.com>
2020-05-14 17:54:21 +0300

    responsive filter menu

--- script/script.js
+++ script/script.js
@@ -2,11 +2,31 @@ function myFunction() {
   var x = document.getElementById("myNavbar");
   if (x.className === "navbar") {
     x.className += " responsive";
+    document.getElementByClassName('close').style.display = "block";
   } else {
+    document.getElementsByClassName('close').style.display = "none";
     x.className = "navbar";
   }
 }
 
-function myG() {
-  document.getElementById("myDIV").innerHTML = "Hello World";
+function myCloseFunction() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar responsive") {
+    x.className = "navbar";
+    document.getElementByClassName('close').style.display = "block";
+  } else {
+    document.getElementsByClassName('close').style.display = "none";
+    x.className = "navbar";
+  }
+}
+
+function myFunctionS() {
+  var x = document.getElementById('show').style.display;
+  if (x === 'none') {
+    document.getElementById('show').style.display = 'block';
+  }
+  else {
+    document.getElementById('show').style.display = 'none';
+
+  }
 }
